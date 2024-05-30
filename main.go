19665553a43dfcadd909d757483b5cac17e287be
C18ner <zeinaliali.mail@gmail.com>
2024-05-30 19:05:46 +0330

    Update main.go

--- main.go
+++ main.go
@@ -122,7 +122,7 @@ func main() {
 							for i := 0; i < len(myconfigs); i++ {
 								if myconfigs[i] != "" {
 									re := regexp.MustCompile(regex_value)
-									myconfigs[i] = strings.ReplaceAll(myconfigs[i], (myconfigs), "")
+									myconfigs[i] = strings.ReplaceAll(myconfigs[i], "\n", "")
 									match := re.FindStringSubmatch(myconfigs[i])
 									if len(match) >= 1 {
 										if proto_regex == "ss" {
