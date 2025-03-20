97bc645538147844649f08a5115bc9bc068e5af8
C18ner <zeinaliali.mail@gmail.com>
2025-03-20 04:42:48 +0330

    Update main.go

--- main.go
+++ main.go
@@ -674,11 +674,11 @@ func main() {
 
 		messages := doc.Find(".tgme_widget_message_wrap").Length()
 		link, exist := doc.Find(".tgme_widget_message_wrap .js-widget_message").Last().Attr("data-post")
-		if messages < 100 && exist {
+		if messages < 20 && exist {
 			number := strings.Split(link, "/")[1]
 			fmt.Println(number)
 
-			doc = GetMessages(100, doc, number, channels[i])
+			doc = GetMessages(20, doc, number, channels[i])
 		}
 
 		if all_messages {
